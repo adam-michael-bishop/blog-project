f845361f9f003ac693c2c0766eab7d5cad5b95fc
adam-michael-bishop <adam.michael.bishop@gmail.com>
2024-08-06 14:58:00 -0500

    init commit

---
+++ webapp/src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Typography variant='h1'>
+        Hello World
+      </Typography>
+    </>
+  )
+}
+
+export default App
