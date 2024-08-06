d4d05d3efd7ac22d4160a521c32750a29f3d7f44
adam-michael-bishop <adam.michael.bishop@gmail.com>
2024-08-06 16:17:10 -0500

    Create navbar

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,20 @@
-import { Typography } from '@mui/material'
+import { Box, Button, Grid, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Typography variant='h1'>
-        Hello World
-      </Typography>
-    </>
+    <Box sx={{width: '100vw', height: '100vh'}}>
+      <Box sx={{height: '10vh', backgroundColor: 'gray', overflow: 'hidden'}}>
+        <Grid sx={{padding: '.5em'}} columns={12} display={'flex'} direction={'row'}>
+          <Box >
+            <Button size='medium' variant='contained'>Sidebar</Button>
+          </Box>
+          <Box >
+            <Typography>Logo</Typography>
+          </Box>
+        </Grid>
+      </Box>
+    </Box>
   )
 }
 
