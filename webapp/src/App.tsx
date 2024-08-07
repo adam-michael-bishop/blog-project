d93272dafe6837073a0b38fbd68e3e52036a9597
adam-michael-bishop <adam.michael.bishop@gmail.com>
2024-08-07 16:06:31 -0500

    Add multiple features - add ThemeProvider wrapper to apply light and dark themes - add functionality to check for user preference of dark mode theme - add Redux store to access theme mode

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,20 +1,51 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box, CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './state/store';
+import { useMemo } from 'react';
+import './App.css';
+
+/* Page Imports */
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import { setThemeMode } from './state/themeModeSlice';
+
 
 function App() {
+  const mode = useSelector((state: RootState) => state.themeMode.mode);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const dispatch = useDispatch();
+
+  if (prefersDarkMode && mode === undefined) {
+    dispatch(setThemeMode('dark'))
+  }
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
-    <Box sx={{width: '100vw', height: '100vh'}}>
-      <Box sx={{height: '10vh', backgroundColor: 'gray', overflow: 'hidden'}}>
-        <Grid sx={{padding: '.5em'}} columns={12} display={'flex'} direction={'row'}>
-          <Box >
-            <Button size='medium' variant='contained'>Sidebar</Button>
-          </Box>
-          <Box >
-            <Typography>Logo</Typography>
-          </Box>
-        </Grid>
-      </Box>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Box sx={{width: '100vw', height: '100vh'}}>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='about' element={<About />}/>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
